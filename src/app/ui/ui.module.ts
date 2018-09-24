8de46592a2f62a7d564c9de552b6715e18459bcf
unknown <abdelaziz.tleti@gmail.com>
2018-09-24 15:34:49 +0200

    ajouter les modules ui

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UiComponent } from './containers/ui/ui.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [UiComponent, NavComponent, FooterComponent, HeaderComponent],
+  // permettre l'acces au comosent par les autres modules
+  exports: [
+      UiComponent
+  ]
 })
 export class UiModule { }
