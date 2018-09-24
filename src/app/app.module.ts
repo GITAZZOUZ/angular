34612e9cd87a0708a989503a36e2e6db07eb5d29
unknown <abdelaziz.tleti@gmail.com>
2018-09-24 14:45:34 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { LoginModule } from './login/login.module';
+import { UiModule } from './ui/ui.module';
+import { PageNotFoundModule } from './page-not-found/page-not-found.module';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    UiModule,
+    LoginModule,
+    PageNotFoundModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
